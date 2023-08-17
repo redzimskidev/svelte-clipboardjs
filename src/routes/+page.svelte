22e3310ab2d463198e9063c614060b2dbebfe0e1
redzimskidev <11840988+redzimskidev@users.noreply.github.com>
2023-08-17 17:10:55 +0200

    Added broadcast channel communication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
+<script>
+  let message = 'Nothing copied yet.';
+  const bc = new BroadcastChannel('clipboard');
+  bc.onmessage = (event) => {
+    if (event.data.success) {
+      message = 'Succesfully copied at ' + new Date().toLocaleTimeString();
+    } else {
+      message = 'Failed to copy at ' + new Date().toLocaleTimeString();
+    }
+  };
+</script>
+
 <div id="container">
   <h1>Look at those awesome pictures!</h1>
   <img src="https://picsum.photos/200/200" alt="Random" />
@@ -6,3 +18,4 @@
 </div>
 
 <button class="btn" data-clipboard-target="#container">Copy</button>
+<p>{message}</p>
